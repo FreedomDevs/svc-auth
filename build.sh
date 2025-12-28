86d131db9eb04e45ad2363d6386618954e5493c1
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-12-29 00:35:23 +0300

    Всё объеденил в 1 удобный скрипт

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cmake -G Ninja -S . -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build -j$(nproc)
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-cmake -G Ninja -S . -B build -DCMAKE_BUILD_TYPE=Debug
-cmake --build build -j$(nproc)
-
-cd build
-gdb --batch -ex run -ex bt -ex quit --args ./svc-auth
