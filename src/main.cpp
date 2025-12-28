30b19c8fda07e9749c9ecf172e8cfd18edbe65b4
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-12-28 14:54:50 +0300

    Теперь он слушает все адреса сразу

--- src/main.cpp
+++ src/main.cpp
@@ -23,15 +23,27 @@ int main() {
 
   app()
       .setBeforeListenSockOptCallback([](int fd) {
-        LOG_INFO << "setBeforeListenSockOptCallback:" << fd;
+        LOG_INFO << "setBeforeListenSockOptCallback: " << fd;
+
+        // Включаем TCP_FASTOPEN
         int enable = 1;
         if (setsockopt(fd, IPPROTO_TCP, TCP_FASTOPEN, &enable,
                        sizeof(enable)) == -1) {
           LOG_INFO << "setsockopt TCP_FASTOPEN failed";
         }
+
+        // Для IPv6 сокетов разрешаем dual-stack
+        int off = 0;
+        if (setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, &off, sizeof(off)) ==
+            -1) {
+          LOG_INFO << "setsockopt IPV6_V6ONLY failed";
+        }
       })
       .setAfterAcceptSockOptCallback([](int) {});
 
-  LOG_INFO << "Server running on 127.0.0.1:9005";
-  app().addListener("127.0.0.1", 9007).run();
+  LOG_INFO << "Server running on all interfaces: 0.0.0.0 and :: on port 9007";
+
+  app().addListener("::", 9007);
+
+  app().run();
 }
