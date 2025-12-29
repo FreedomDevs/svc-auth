f01ad4d415adaca816feafcc6b2556ad667bade2
Foksif <knenrerp@gmail.com>
2025-12-29 14:10:09 +0700

    Чёто поделал

--- include/codes.hpp
+++ include/codes.hpp
@@ -2,18 +2,20 @@
 
 #include <string_view>
 
-enum class Codes { AUTH_SUCCESS, AUTH_FAILED, USER_NOT_FOUND, TOKEN_GENERATE_ERR };
+struct Codes {
+  enum class Code { AUTH_SUCCESS, AUTH_FAILED, USER_NOT_FOUND, TOKEN_GENERATE_ERR };
 
-inline constexpr std::string_view to_string(Codes code) {
-  switch (code) {
-  case Codes::AUTH_SUCCESS:
-    return "AUTH_SUCCESS";
-  case Codes::AUTH_FAILED:
-    return "AUTH_FAILED";
-  case Codes::USER_NOT_FOUND:
-    return "USER_NOT_FOUND";
-  case Codes::TOKEN_GENERATE_ERR:
-    return "TOKEN_GENERATE_ERR";
+  static inline constexpr std::string_view to_string(Code code) {
+    switch (code) {
+    case Code::AUTH_SUCCESS:
+      return "AUTH_SUCCESS";
+    case Code::AUTH_FAILED:
+      return "AUTH_FAILED";
+    case Code::USER_NOT_FOUND:
+      return "USER_NOT_FOUND";
+    case Code::TOKEN_GENERATE_ERR:
+      return "TOKEN_GENERATE_ERR";
+    }
+    return "UNKNOWN_CODE";
   }
-  return "UNKNOWN_CODE";
-}
+};
--- include/responseHandler.hpp
+++ include/responseHandler.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "codes.hpp"
 #include <drogon/HttpResponse.h>
+#include <json/json.h>
 
 class ResponseHandler {
 public:
+  static drogon::HttpResponsePtr success(int statusCode, const std::string &message, Codes::Code code, const Json::Value &data = Json::nullValue);
+  static drogon::HttpResponsePtr error(int statusCode, const std::string &message, Codes::Code code);
 };
