fdfb94ef31ed6376f56bcb07385c2dab62ed12c6
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-12-30 20:01:59 +0300

    Вкусненькие индексы в БД

--- src/DBManager.cpp
+++ src/DBManager.cpp
@@ -21,11 +21,15 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
   expires_at TIMESTAMP NOT NULL
 );
 )";
+const std::string TOKEN_HASH_INDEX = "CREATE UNIQUE INDEX IF NOT EXISTS idx_refresh_tokens_tokenhash ON refresh_tokens(tokenHash);";
+const std::string TOKEN_EXPIRES_AT_INDEX = "CREATE INDEX IF NOT EXISTS idx_refresh_tokens_expires_at ON refresh_tokens(expires_at);";
 
 void initDatabase() {
   client = drogon::orm::DbClient::newPgClient(DB_CONNECT_STRING, DB_CONNECTIONS_POOL_SIZE);
   client->execSqlSync(INTEGRATIONS_TABLE);
   client->execSqlSync(REFRESH_TOKENS_TABLE);
+  client->execSqlSync(TOKEN_HASH_INDEX);
+  client->execSqlSync(TOKEN_EXPIRES_AT_INDEX);
 }
 void closeDatabase() { client = nullptr; }
 
