11dff30216bb0474a76a2c799ecbd483230b9b6e
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-12-30 19:00:04 +0300

    Мини рефактор main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,23 @@
 #include <drogon/drogon.h>
 using namespace drogon;
 
+void configurateSocket(int fd) {
+  // Включаем TCP_FASTOPEN
+  int enable = 1;
+  if (setsockopt(fd, IPPROTO_TCP, TCP_FASTOPEN, &enable, sizeof(enable)) == -1) {
+    LOG_INFO << "setsockopt TCP_FASTOPEN failed";
+  }
+
+  // Вклчаем dual-stack, IPv6 + IPv4
+  int off = 0;
+  if (setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, &off, sizeof(off)) == -1) {
+    LOG_INFO << "setsockopt IPV6_V6ONLY failed";
+  }
+}
+
 int main() {
-  app()
-      .setBeforeListenSockOptCallback([](int fd) {
-        LOG_INFO << "setBeforeListenSockOptCallback: " << fd;
+  app().setBeforeListenSockOptCallback(configurateSocket).setAfterAcceptSockOptCallback([](int) {}).addListener("::", 9007);
 
-        // Включаем TCP_FASTOPEN
-        int enable = 1;
-        if (setsockopt(fd, IPPROTO_TCP, TCP_FASTOPEN, &enable, sizeof(enable)) == -1) {
-          LOG_INFO << "setsockopt TCP_FASTOPEN failed";
-        }
-
-        // Вклчаем dual-stack, IPv6 + IPv4
-        int off = 0;
-        if (setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, &off, sizeof(off)) == -1) {
-          LOG_INFO << "setsockopt IPV6_V6ONLY failed";
-        }
-      })
-      .setAfterAcceptSockOptCallback([](int) {});
-
-  LOG_INFO << "Server running on all interfaces: *:9007";
-  app().addListener("::", 9007).run();
+  LOG_INFO << "Server running on: *:9007";
+  app().run();
 }
