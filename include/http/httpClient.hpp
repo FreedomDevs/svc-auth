a6b41dd9d3790746122e1d77cffa2b8ac30b28fd
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-02-22 18:04:13 +0300

    Исправил

--- include/http/httpClient.hpp
+++ include/http/httpClient.hpp
@@ -14,25 +14,23 @@ public:
       : client_(drogon::HttpClient::newHttpClient(baseUrl)), retries_(retries), timeout_(timeout) {}
 
   template <typename Resp> drogon::Task<HttpResult<Resp>> get(const std::string &path) {
-    return request<std::monostate, Resp>(drogon::Get, path, nullptr);
+    return request<std::monostate, Resp>(drogon::Get, path, std::monostate{});
   }
 
-  template <typename Req, typename Resp> drogon::Task<HttpResult<Resp>> post(const std::string &path,
-                            const Req &body) {
-    return request<Req, Resp>(drogon::Post, path, &body);
-}
+  template <typename Req, typename Resp> drogon::Task<HttpResult<Resp>> post(const std::string &path, const Req &body) {
+    return request<Req, Resp>(drogon::Post, path, body);
+  }
 
   template <typename Req, typename Resp> drogon::Task<HttpResult<Resp>> put(const std::string &path, const Req &body) {
-    return request<Req, Resp>(drogon::Put, path, &body);
+    return request<Req, Resp>(drogon::Put, path, body);
   }
 
   drogon::Task<HttpResult<std::monostate>> del(const std::string &path) {
-    return request<std::monostate, std::monostate>(drogon::Delete, path, nullptr);
+    return request<std::monostate, std::monostate>(drogon::Delete, path, std::monostate{});
   }
 
 private:
-  template <typename Req, typename Resp>
-  drogon::Task<HttpResult<Resp>> request(drogon::HttpMethod method, std::string path, const Req *body) {
+  template <typename Req, typename Resp> drogon::Task<HttpResult<Resp>> request(drogon::HttpMethod method, std::string path, Req body) {
     try {
       const double timeoutSec = std::chrono::duration_cast<std::chrono::duration<double>>(timeout_).count();
 
@@ -41,6 +39,7 @@ private:
         req->setMethod(method);
         req->setPath(path);
 
+        LOG_INFO << "3";
         if constexpr (!std::is_same_v<Req, std::monostate>) {
           req->setContentTypeCode(drogon::CT_APPLICATION_JSON);
 
@@ -52,8 +51,9 @@ private:
           builder["indentation"] = ""; // Гарантируем отсутствие отступов
 
           // Сериализуем Json::Value безопасно
-          req->setBody(Json::writeString(builder, *body));
+          req->setBody(Json::writeString(builder, body));
         }
+        LOG_INFO << "4";
 
         drogon::HttpResponsePtr resp = co_await client_->sendRequestCoro(req, timeoutSec);
 
